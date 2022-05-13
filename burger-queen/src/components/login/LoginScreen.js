493dbc6ae0e75f39c5c547122024dacd11b01787
Maria Saavedra <cotesaavedra@github.com>
2022-05-13 15:43:46 -0400

    Petición lista de productos al clickear un boton

--- burger-queen/src/components/login/LoginScreen.js
+++ burger-queen/src/components/login/LoginScreen.js
@@ -1,10 +1,14 @@
-import  boton from './components/button/ButtonScreen'
+import React from 'react';
+// import ReactDOM from 'react-dom/client'; 
+import LoginBtn from '../button/ButtonScreen';
+
+
 export const LoginScreen= () =>{
   return (
     <div>
-      
-      <h1>LoginScreen</h1>
 
+      <h1>LoginScreen</h1>
+      <LoginBtn />
       </div>
   )
 }

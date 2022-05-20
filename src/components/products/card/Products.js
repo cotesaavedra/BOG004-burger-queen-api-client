c3d41426577967292d859b307b2580b303e606a0
Maria Saavedra <cotesaavedra@github.com>
2022-05-20 12:15:42 -0400

    Se agregó propiedad quality al producto, se creó la card por producto, contador

---
+++ src/components/products/card/Products.js
@@ -0,0 +1,33 @@
+import Counter from "../counter/Counter";
+
+
+const ProductCard = ({ products, setProducts }) => {
+    console.log(products)
+
+    const returnCounter = (product, value) => {
+        console.log('value', value);
+        // console.log('product', product)
+        let productsClone = Object.assign([], products); // clon del estado para actualizar su información
+        productsClone.forEach(element => {
+            // console.log('cantidad', element.quantity)
+            if (element.id === product.id){
+                element.quantity = value; 
+            }
+        });
+        console.log('product', products)
+        // actualizar el quantity del producto
+        
+        setProducts(productsClone);
+    }
+
+    return (
+        products.map((product) => (
+            <div className='product' key={product.id}>
+                <h6>{product.name}</h6>
+                <p>Precio: ${product.price}</p>
+                <Counter amount={product.quantity} retorno={(valor) => returnCounter(product, valor)}></Counter> 
+            </div>
+        ))
+    )
+}
+export default ProductCard;
--- src/components/waiter/WaiterScreen.js
+++ src/components/waiter/WaiterScreen.js
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTh, faCutlery } from '@fortawesome/free-solid-svg-icons';
 import { NavLeft } from '../ui/left/NavLeft';
+import ProductCard from '../products/card/Products';
 
 
 export const WaiterScreen = () => {
@@ -14,11 +15,14 @@ export const WaiterScreen = () => {
       method: 'GET', // or 'PUT'
       headers: {
         'Content-Type': 'application/json',
-        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY1MzAxMDI2NSwiZXhwIjoxNjUzMDEzODY1LCJzdWIiOiIyIn0.0LsOJDVn7u3cdJkbo1l00QEd0B9eKP5vdyxVRQ5Fhj4',
+        'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImdyYWNlLmhvcHBlckBzeXN0ZXJzLnh5eiIsImlhdCI6MTY1MzA2MjU2NSwiZXhwIjoxNjUzMDY2MTY1LCJzdWIiOiIyIn0.cUo00DtcHfjeWk6oc4FMWQ2APbqbrNKZj4ia9hq5ciQ',
       }
     })
       .then(response => response.json())
       .then(data => {
+        data.forEach((product) => {
+          product.quantity = 0;
+        })
         if (typeof data === 'object' && data.length > 0) {
           setProducts(data);
         }
@@ -38,12 +42,13 @@ export const WaiterScreen = () => {
         <h2>Menú</h2>
         {/* <p>{products}</p> */}
         <div className='products-container'>
-          {products.map((product) => (
+          <ProductCard products={ products } setProducts={setProducts}></ProductCard>
+          {/* {products.map((product) => (
             <div className='product'>
               <h6 key={product.id + 'name'}>{product.name}</h6>
               <p key={product.id + 'price'}>Precio: ${product.price}</p>
             </div>
-        ))}
+          ))} */}
         </div>
       </Col>
 
